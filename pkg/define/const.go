45f8a90db4493cf6d36e4eb8b1bdb2a90c879a3b
CAT <14349453+ihexon@users.noreply.github.com>
2025-05-13 12:09:44 +0800

    refactor(handler): downgrade the ffmpeg from 7 to 6 for compatibility (#23)

--- pkg/define/const.go
+++ pkg/define/const.go
@@ -1,17 +1,29 @@
 package define
 
 const (
-	FFMPEG                 = "ffmpeg"
-	FFPROBE                = "ffprobe"
-	InstallFFMPEG          = "install_ffmpeg"
-	StudioDir              = ".oomol-studio"
-	HostShared             = "host-shared"
-	FFReleaseURLForVentura = "https://github.com/jellyfin/jellyfin-ffmpeg/releases/download/v7.1.1-2/jellyfin-ffmpeg_7.1.1-2_portable_macarm64-gpl.tar.xz"
-	FFMSha256ForVentura    = "8ff4ed4eba151346d6d1ee894bbfcbd9f507dc82b34260f4567954136449001d"
+	ARCH = "arm64"
+
+	FFMPEGPkgName = "ffmpeg_macos_arm64"
+
+	// FFMPEG VERSION v7.1.1 info
+	FFMPEG7Version = "v7.1.1"
+	FF7ReleaseURL  = "https://github.com/jellyfin/jellyfin-ffmpeg/releases/download/v7.1.1-2/jellyfin-ffmpeg_7.1.1-2_portable_macarm64-gpl.tar.xz"
+	FF7Sha256      = "8ff4ed4eba151346d6d1ee894bbfcbd9f507dc82b34260f4567954136449001d"
+
+	// 	// FFMPEG VERSION v6.0.1 info
+	FFMPEG6Version = "v6.0.1"
+	FF6ReleaseURL  = "https://github.com/jellyfin/jellyfin-ffmpeg/releases/download/v6.0.1-8/jellyfin-ffmpeg_6.0.1-8_portable_macarm64-gpl.tar.xz"
+	FF6Sha256      = "efce8779d5f35127ec7dcd500669e00a2e13f2b099c7bf11ab5acee467dc5d57"
 )
 
 const (
-	DYLD_LIBRARY_PATH = "DYLD_LIBRARY_PATH" //nolint:stylecheck
+	InstallFFMPEGVersion7 = "install_ffmpeg_7"
+	InstallFFMPEGVersion6 = "install_ffmpeg_6"
+	StudioDir             = ".oomol-studio"
+	HostShared            = "host-shared"
+
+	FFMPEGBin  = "ffmpeg"
+	FFPROBEBin = "ffprobe"
 )
 
 var (
